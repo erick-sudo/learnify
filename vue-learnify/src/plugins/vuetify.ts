3056db93317bb3fac4c7e7873391c48a3a84d24c
erick-sudo <erickochieng766@gmail.com>
2024-11-29 09:34:37 +0300

    Initial Vue Setup

---
+++ vue-learnify/src/plugins/vuetify.ts
@@ -0,0 +1,33 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+// Composables
+import { createVuetify } from "vuetify";
+
+import colors from "vuetify/util/colors";
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  theme: {
+    defaultTheme: "dark",
+    themes: {
+      dark: {
+        colors: {
+          primary: colors.deepOrange.accent3,
+          primaryContainer: colors.deepOrange.darken3,
+          primaryVariant: colors.deepOrange.lighten3,
+          onPrimaryContainer: colors.deepOrange.lighten4,
+          secondary: colors.grey.darken2,
+          // error: colors.red.darken1,
+        },
+      },
+    },
+  },
+});
